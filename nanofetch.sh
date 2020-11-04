515544b8626c229ca1cc0baccbf2ccefbe3657fb
Leah Nicole <51840792+littlemisssynth@users.noreply.github.com>
2020-11-04 13:24:29 +0000

    Update nanofetch.sh

--- nanofetch.sh
+++ nanofetch.sh
@@ -7,13 +7,11 @@ memkb=$(echo $memraw | sed 's/MemTotal: //g; s/kB//g')
 mem=$(expr $memkb / 1024)
 memrawfree=$(grep -m 1 -i 'MemAvailable' /proc/meminfo)
 memfkb=$(echo $memrawfree | sed 's/MemAvailable: //g; s/kB//g')
-memfree=$(expr $memfkb / 1024)
 distro=$(grep -m 1 -i 'NAME=' /etc/os-release | sed 's/NAME=//g;s/"//g;s/PRETTY_//g')
 
 # Print output
 
-echo Kernel: $kernel
+echo Kernel: $kernel (Darwin)
 echo Distro: $distro
 echo Shell: $SHELL
 echo Mem: $mem\mb
-echo Free Mem: $memfree\mb
