aaebc90b889d4a73ac87860d52cfc1e7a357d832
Leah Nicole <51840792+littlemisssynth@users.noreply.github.com>
2020-11-04 13:02:56 +0000

    Update nanofetch.sh
    removed features that are not supported by iSH

--- nanofetch.sh
+++ nanofetch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Simple fetch script
 # Get variables
-cpu=$(grep -m 1 -i 'model name' /proc/cpuinfo)
 kernel=$(uname -r)
 memraw=$(grep -m 1 -i 'MemTotal' /proc/meminfo)
 memkb=$(echo $memraw | sed 's/MemTotal: //g; s/kB//g')
@@ -9,7 +8,6 @@ mem=$(expr $memkb / 1024)
 memrawfree=$(grep -m 1 -i 'MemAvailable' /proc/meminfo)
 memfkb=$(echo $memrawfree | sed 's/MemAvailable: //g; s/kB//g')
 memfree=$(expr $memfkb / 1024)
-uptime=$(uptime -p | sed 's/up //g')
 distro=$(grep -m 1 -i 'NAME=' /etc/os-release | sed 's/NAME=//g;s/"//g;s/PRETTY_//g')
 
 # Print output
@@ -19,6 +17,3 @@ echo Distro: $distro
 echo Shell: $SHELL
 echo Mem: $mem\mb
 echo Free Mem: $memfree\mb
-echo $cpu | sed 's/model name :/CPU:/g'
-echo Uptime: $uptime
-
